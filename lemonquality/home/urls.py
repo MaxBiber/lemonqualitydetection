3f68318cf8dfed0714b66b0c8a9c0c2758817c8b
Max Biber <max.biber@orange.fr>
2024-10-09 17:00:19 +0200

    website design

--- lemonquality/home/urls.py
+++ lemonquality/home/urls.py
@@ -5,5 +5,7 @@ from home import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact')
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('registration/', views.registration, name='registration')
 ]
